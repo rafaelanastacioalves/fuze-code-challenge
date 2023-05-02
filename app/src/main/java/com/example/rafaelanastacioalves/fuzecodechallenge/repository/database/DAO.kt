f0bab0dd9ebbdebce0c6ebdb08767d51143f1c0f
Rafael Anastácio Alves <rafaelanastacioalves@gmail.com>
2023-05-02 04:20:05 -0300

    feat: remove database
    too much technical issues to solve for a code challenge

--- app/src/main/java/com/example/rafaelanastacioalves/fuzecodechallenge/repository/database/DAO.kt
+++ app/src/main/java/com/example/rafaelanastacioalves/fuzecodechallenge/repository/database/DAO.kt
@@ -4,19 +4,10 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import com.example.rafaelanastacioalves.fuzecodechallenge.domain.entities.Match
 
 @Dao
 interface DAO {
 
-    @Query("SELECT * FROM mainentity")
-    fun getMainEntityList(): List<MainEntity>
 
-    @Insert
-    fun saveMainEntityList(resultData: List<MainEntity>?)
-
-    @Delete
-    fun delete(mainEntity: MainEntity)
-
-    @Query("DELETE FROM mainentity")
-    fun delteAllMainEntities()
 }
